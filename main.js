1310d930a08dce65f74b8d97268c555f7f46bf3d
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-09-13 18:23:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 "use strict";
-var items = [ "Salmon", "Pine Nuts" ];
+var items = [ "Salmon", "Pine Nuts", "Yellow Squash" ];
 
-var dish = React.createElement("ul", {id: "id", className: "class", "data-type": "type"},
-  items.map( (item, index) => React.createElement("li", {key: index}, item)));
-ReactDOM.render(dish, document.getElementById("react-container"));
+// CreateClass Components
+{
+   const ingredients = React.createClass({
+       displayName: "htmlTagName",
+       objMethodName(item, i) {
+           return React.createElement("li", {key: i}, item);
+       },
+       render() {
+           return React.createElement("ul", {className: "anyClassName"},
+               this.props.name.map(this.objMethodName));   // get access to obj props
+       }
+   });
+   const list = React.createElement(ingredients, {name: items}, null); // pass props to child
+   ReactDOM.render(list, document.getElementById("react-createClass"));
+}
+// Class Components
+{
+   class Ingredient extends React.Component {
+       objMethodName(item, i) {
+           return React.createElement("li", {key: i}, item);
+       }
+       render() {
+           return React.createElement("ul", {className: "anyClassName"},
+               this.props.name.map(this.objMethodName));
+       }
+   }
+   const ingredient = new Ingredient();
+   const newList = React.createElement(ingredient, {name: items}, null);
+   ReactDOM.render(newList, document.getElementById("react-component"));
+}
+// Stateless Functional Components
+{
+    
+}
